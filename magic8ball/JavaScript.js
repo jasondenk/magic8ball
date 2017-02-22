67a5b0aca2c8d64fcf49ab66356f78da74754c83
Jason Denk <jdenk9@gmail.com>
2017-02-22 15:14:00 -0500

    initial commit

---
+++ magic8ball/JavaScript.js
@@ -0,0 +1,17 @@
+﻿var phrases = ['You will eat well', 'Be cautious around lions', 'You will be rich', 'You will be poor','You will fly someday','Watch your step','Own your consciousness', 'Happy Day!'];
+
+function generatePhrase() {
+    var randomValue = Math.floor(phrases.length * Math.random());
+    document.getElementById("eight").innerHTML= phrases[randomValue];
+}
+
+
+
+$(document).click(function () {
+    $("#ball").effect("shake");
+});
+
+
+
+
+
